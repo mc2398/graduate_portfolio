0c1c84aa68be8d572f97e10aec66403c4fa2bd98
mc2398 <148909533+mc2398@users.noreply.github.com>
2025-03-30 19:07:13 -0400

    js experimentation

--- scripts.js
+++ scripts.js
@@ -12,3 +12,35 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+  function yassify() {
+  document.getElementByClass("libguide-head-background").style.color =
+  "#F51387";
+}
+// function makeBoring() {
+//     document.getElementsByClass("accordion").style.color = "black";
+// }
+//   const element = document.getElementById("accordion");
+  
+//   element.addEventListener("click", function() {
+//   // Generate a random color
+//   const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+ 
+
+//   // Change the background color of the element
+//   element.style.backgroundColor = randomColor;
+//   });
+
+// // var acc = document.getElementsByClassName("accordion");
+// // var i;
+
+// for (i = 0; i < acc.length; i++) {
+//   acc[i].addEventListener("click", function() {
+//     this.classList.toggle("active");
+//     var panel = this.nextElementSibling;
+//     if (panel.style.maxHeight) {
+//       panel.style.maxHeight = null;
+//     } else {
+//       panel.style.maxHeight = panel.scrollHeight + "px";
+//     } 
+//   });
+// }
